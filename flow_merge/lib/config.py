5748e1ca0ede39e52298476d3599b3e8ae96eb5b
databello <karolus@databello.com>
2024-05-22 15:52:24 +0300

    changes from the call

--- flow_merge/lib/config.py
+++ flow_merge/lib/config.py
@@ -1,12 +1,17 @@
 import os
 
 
-class CentralConfig:
+class ApplicationConfig:
     def __init__(self):
         self.hf_token = os.getenv("HF_TOKEN")
+        self.device = "TBD"
+        ## FIXME: bring device setting here and read from here at the implementation level, skip passing
+        ## Beware some places don't mention device but have "cpu", FIND THEM
+        ## We also have mixed DeviceIdentifier Literal[str] = 'cpu', 'cuda'
+        ## Hugging Face's DeviceSomething - competing type ?
 
     def set_hf_token(self, token: str):
         self.hf_token = token
 
 
-config = CentralConfig()
+config = ApplicationConfig()
