32c6f9e92f10b4083bef21da32d038417e2cc0dc
sariola <karolus@flowrite.com>
2024-05-30 22:59:51 +0300

    refactor: handling embed, norm and lm_head wo/ hardcoding their full path

--- flow_merge/lib/loaders/normalizer.py
+++ flow_merge/lib/loaders/normalizer.py
@@ -19,12 +19,12 @@ class NormalizationRunner:
         }
         self.transformations = [self.ensure_base_model]
 
-    def ensure_base_model(self, config: Dict[str, Any]) -> Dict[str, Any]:
-        sources = config.get("sources", [])
+    def ensure_base_model(self, slice: Dict[str, Any]) -> Dict[str, Any]:
+        sources = slice.get("sources", [])
         if not any((src.get("base_model") == True) for src in sources):
             sources[0]["base_model"] = True
-        config["sources"] = sources
-        return config
+        slice["sources"] = sources
+        return slice
 
     def create_slice(
         self, sources: List[Dict[str, Any]], layer: str, merge_method: str
@@ -36,75 +36,100 @@ class NormalizationRunner:
             }
         }
 
-    def process_slice(self, slice: Dict[str, Any]) -> List[Dict[str, Any]]:
-        sources = slice["sources"]
-        merge_method = slice.get("merge_method")
-        layers_to_keep = slice.get("layers")
-        layers = list(self.layers.keys())
+    def process_slice(
+        self, slice: Dict[str, Any], normalized_data
+    ) -> List[Dict[str, Any]]:
+        special_layer_names = [
+            item
+            for item in list(self.layers.keys())
+            if "{" not in item and "}" not in item
+        ]
+        layer_name_templates = [
+            item for item in list(self.layers.keys()) if "{" in item and "}" in item
+        ]
         layer_types = list(self.layers.values())
 
         if "range" in slice:
             start, end = slice["range"]
             slices = [
-                self.create_slice(sources, lt.format(layer_index=i), merge_method)
+                self.create_slice(
+                    slice["sources"], lnt.format(layer_index=i), slice["merge_method"]
+                )
                 for i in range(start, end + 1)
-                for lt in layers
+                for lnt in layer_name_templates
             ]
         elif "layer" in slice:
-            slices = [self.create_slice(sources, slice["layer"], merge_method)]
-
-        ## TODO: bring the missing layers from the base_model if we have a
-        ## layer filter
+            slices = [
+                self.create_slice(
+                    slice["sources"], slice["layer"], slice["merge_method"]
+                )
+            ]
         base_model = None
-        for search_slice in slices:
-            for src in search_slice["slice"]["sources"]:
+        layers_to_keep = slice.get("layers", None)
+        layers_to_process = list(
+            set(
+                [
+                    lt
+                    for lt in layer_types
+                    if lt not in layers_to_keep + special_layer_names
+                ]
+            )
+        )
+        for i, slice_entry in enumerate(slices):
+            new_sources = []
+            for src in slice_entry["slice"]["sources"]:
                 if src.get("base_model", False):
-                    base_model = src.get("model")
-
-        new_slices = []
-        for write_slice in slices:
-            for layer_type in layer_types:
-                if (
-                    layer_type in write_slice["slice"]["sources"][0]["layer"]
-                    and layer_type in layers_to_keep
-                ):
-                    new_slices.append(write_slice)
-                    continue
+                    base_model = src["model"]
+                if any(layer_type in src["layer"] for layer_type in layers_to_process):
+                    src["model"] = base_model
+                    slice_entry["slice"]["merge_method"] = "passthrough"
+                    if src.get("base_model", False):
+                        new_sources.append(src)
                 else:
-                    for layer_template_name in layers:
-                        new_slices.append(
-                            {
-                                "slice": {
-                                    "merge_method": "passthrough",
-                                    "sources": [
-                                        {
-                                            "model": base_model,
-                                            "base_model": True,
-                                            "layer": layer_template_name,
-                                        }
-                                    ],
-                                }
-                            }
-                        )
+                    new_sources.append(src)
 
-        return new_slices
+            slice_entry["slice"]["sources"] = new_sources
+            slices[i] = slice_entry
+        return slices
 
-    def apply_transformations(self, config: Dict[str, Any]) -> Dict[str, Any]:
-        return reduce(lambda c, t: t(c), self.transformations, config)
+    def process_special_layers(self, normalized_data) -> List[Dict[str, Any]]:
+        special_layer_names = [
+            item
+            for item in list(self.layers.keys())
+            if "{" not in item and "}" not in item
+        ]
+        for special_layer_name in special_layer_names:
+            if "embed" in special_layer_name:
+                embed_slice = self.create_slice(
+                        normalized_data[0]["slice"]["sources"],
+                        special_layer_name,
+                        normalized_data[0]["slice"]["merge_method"],
+                    )
+            if "norm" in special_layer_name:
+                norm_slice = self.create_slice(
+                        normalized_data[len(normalized_data)-1]["slice"]["sources"],
+                        special_layer_name,
+                        normalized_data[len(normalized_data)-1]["slice"]["merge_method"],
+                    )
+            if "lm_head" in special_layer_name:
+                lm_head_slice = self.create_slice(
+                    normalized_data[len(normalized_data)-1]["slice"]["sources"],
+                    special_layer_name,
+                    normalized_data[len(normalized_data)-1]["slice"]["merge_method"],
+                    )
+        slices = [embed_slice, norm_slice, lm_head_slice]
+        return slices
+
+    def apply_transformations(self, slice: Dict[str, Any]) -> Dict[str, Any]:
+        return reduce(lambda c, t: t(c), self.transformations, slice)
 
     def normalize(self, raw_data: List[Dict[str, Any]]) -> List[Dict[str, Any]]:
         normalized_data = []
-        for config in raw_data:
-            config = self.apply_transformations(config)
-            normalized_data.extend(self.process_slice(config))
+        for slice in raw_data:
+            slice = self.apply_transformations(slice)
+            normalized_data.extend(self.process_slice(slice, normalized_data))
+            normalized_data.extend(self.process_special_layers(normalized_data))
         return normalized_data
-
-
-def test_expand_range(
-    config: Dict[str, Any], runner: NormalizationRunner
-) -> List[Dict[str, Any]]:
-    config = runner.apply_transformations(config)
-    return runner.handle_config(config)
 
 
 def display_slices(slices: List[Dict[str, Any]]):
