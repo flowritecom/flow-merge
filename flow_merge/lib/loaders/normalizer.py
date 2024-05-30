bc3505e7cb9d460fd5d825b5b95f6536a968c13e
sariola <karolus@flowrite.com>
2024-05-30 20:04:05 +0300

    refactor: filling in base_model layers with passthrough layer filter is used

--- flow_merge/lib/loaders/normalizer.py
+++ flow_merge/lib/loaders/normalizer.py
@@ -19,14 +19,14 @@ class NormalizationRunner:
         }
         self.transformations = [self.ensure_base_model]
 
-    def ensure_base_model(self, slice: Dict[str, Any]) -> Dict[str, Any]:
-        sources = slice.get("sources", [])
+    def ensure_base_model(self, config: Dict[str, Any]) -> Dict[str, Any]:
+        sources = config.get("sources", [])
         if not any((src.get("base_model") == True) for src in sources):
             sources[0]["base_model"] = True
-        slice["sources"] = sources
-        return slice
+        config["sources"] = sources
+        return config
 
-    def create_slice_entries(
+    def create_slice(
         self, sources: List[Dict[str, Any]], layer: str, merge_method: str
     ) -> Dict[str, Any]:
         return {
@@ -36,34 +36,58 @@ class NormalizationRunner:
             }
         }
 
-    def create_slices_from_range(
-        self,
-        sources: List[Dict[str, Any]],
-        start: int,
-        end: int,
-        merge_method: str,
-        layers_to_keep: List[str],
-    ) -> List[Dict[str, Any]]:
-        return [
-            self.create_slice_entries(sources, lt.format(layer_index=i), merge_method)
-            for i in range(start, end + 1)
-            for lt, lta in self.layers.items()
-            if layers_to_keep is None or any(lt in lta for lt in layers_to_keep)
-        ]
+    def process_slice(self, slice: Dict[str, Any]) -> List[Dict[str, Any]]:
+        sources = slice["sources"]
+        merge_method = slice.get("merge_method")
+        layers_to_keep = slice.get("layers")
+        layers = list(self.layers.keys())
+        layer_types = list(self.layers.values())
 
-    def expand_range(self, config: Dict[str, Any]) -> List[Dict[str, Any]]:
-        return self.create_slices_from_range(
-            config["sources"],
-            config["range"][0],
-            config["range"][1],
-            config.get("merge_method"),
-            config.get("layers", None),
-        )
+        if "range" in slice:
+            start, end = slice["range"]
+            slices = [
+                self.create_slice(sources, lt.format(layer_index=i), merge_method)
+                for i in range(start, end + 1)
+                for lt in layers
+            ]
+        elif "layer" in slice:
+            slices = [self.create_slice(sources, slice["layer"], merge_method)]
 
-    def process_layer(self, config: Dict[str, Any]) -> Dict[str, Any]:
-        return self.create_slice_entries(
-            config["sources"], config["layer"], config.get("merge_method")
-        )
+        ## TODO: bring the missing layers from the base_model if we have a
+        ## layer filter
+        base_model = None
+        for search_slice in slices:
+            for src in search_slice["slice"]["sources"]:
+                if src.get("base_model", False):
+                    base_model = src.get("model")
+
+        new_slices = []
+        for write_slice in slices:
+            for layer_type in layer_types:
+                if (
+                    layer_type in write_slice["slice"]["sources"][0]["layer"]
+                    and layer_type in layers_to_keep
+                ):
+                    new_slices.append(write_slice)
+                    continue
+                else:
+                    for layer_template_name in layers:
+                        new_slices.append(
+                            {
+                                "slice": {
+                                    "merge_method": "passthrough",
+                                    "sources": [
+                                        {
+                                            "model": base_model,
+                                            "base_model": True,
+                                            "layer": layer_template_name,
+                                        }
+                                    ],
+                                }
+                            }
+                        )
+
+        return new_slices
 
     def apply_transformations(self, config: Dict[str, Any]) -> Dict[str, Any]:
         return reduce(lambda c, t: t(c), self.transformations, config)
@@ -72,21 +96,24 @@ class NormalizationRunner:
         normalized_data = []
         for config in raw_data:
             config = self.apply_transformations(config)
-            if "range" in config:
-                normalized_data.extend(self.expand_range(config))
-            elif "layer" in config:
-                normalized_data.append(self.process_layer(config))
-            else:
-                normalized_data.append(config)
+            normalized_data.extend(self.process_slice(config))
         return normalized_data
+
 
 def test_expand_range(
     config: Dict[str, Any], runner: NormalizationRunner
 ) -> List[Dict[str, Any]]:
     config = runner.apply_transformations(config)
-    return runner.expand_range(config)
+    return runner.handle_config(config)
 
 
 def display_slices(slices: List[Dict[str, Any]]):
     for entry in slices:
         print(json.dumps(entry, indent=2))
+
+
+#################
+## TODO: option to include range and layer in the sources directly
+## TODO: but have top level as option too to apply for them
+
+## TODO: allow shorter version of the template language if filter is present
